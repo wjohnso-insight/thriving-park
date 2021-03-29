c938cd58f5520b7034c54c032b645d8ce59ff07d
Will <will.johnston@insight.com>
2021-03-29 14:31:38 -0400

    Adds `navoptions` utility
    `navoptions` is an array that is used to populate the `<LinkTab>`
    elements in `<Header>`.

---
+++ src/utils/navoptions.js
@@ -0,0 +1,11 @@
+//@flow 
+// ! The values in this array are used to populate the main navigation options
+// ! in `<Header>` 
+
+const navoptions : string[] = [
+    'about',
+    'move',
+    'litter'
+];
+
+export default navoptions;
