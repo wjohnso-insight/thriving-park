556cfca6a7ebce38d8c55afad558c82a7587a4e0
Will <will.johnston@insight.com>
2021-03-29 12:47:57 -0400

    Adds breakpoints util
    Added a reference for global breakpoints as an object that can be used
    to configure the `useMediaQueryHook()`

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,10 @@
+//@flow
+
+const breakpoints = {
+    desktop : "(min-width: 1025px)",
+    laptop : "(min-width: 796px)",
+    tablet: "(min-width: 481px)",
+    mobile: "(max-width: 480px)"
+}
+
+export default breakpoints;
