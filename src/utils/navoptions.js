1fddc3819b1bf8f1cc4f2aa364f52c50a3cc89fc
Will <will.johnston@insight.com>
2021-03-30 09:18:21 -0400

    Refactors `navoption`
    I changed navoption from a simple array of values, to an array of
    `Navoption` objects.

--- src/utils/navoptions.js
+++ src/utils/navoptions.js
@@ -2,10 +2,14 @@
 // ! The values in this array are used to populate the main navigation options
 // ! in `<Header>` 
 
-const navoptions : string[] = [
-    'about',
-    'move',
-    'litter'
+export interface Navoption{
+    path: string,
+    markdownVal: string
+}
+
+export const navoptions : Navoption[] = [
+    {path: '/', markdownVal: 'about'},
+    {path: '/move', markdownVal: 'move'},
+    {path: '/litter', markdownVal: 'litter'}
 ];
 
-export default navoptions;
