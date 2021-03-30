6245626dc7360e1f4110170722c3704faea93144
Will <will.johnston@insight.com>
2021-03-30 13:55:44 -0400

    Adds mobile breakpoints to `<LinkTab>`
    I added mobile breakpoints to `<LinkTab>` to make it responsive for
    mobile.

--- src/components/display/linktab/LinkTab.js
+++ src/components/display/linktab/LinkTab.js
@@ -5,12 +5,16 @@
         [√] Be styled like a folder tab
             - border radius
             - drop shadow
-        [] Style if active
+        [√] Style if active
+        [] Make tab display responsive
  */
 //@flow
 import * as React from 'react'
+import breakpoints from '../../../utils/breakpoints'
+import useMediaQuery from '../../../hooks/useMediaQuery'
 
 import './LinkTab.scoped.scss'
+
 type Props = {
     children?: React.Node,
     path: string
@@ -18,8 +22,21 @@ type Props = {
 
 export default function LinkTab(props: Props) : React.Node {
 
+    const { desktop, laptop, tablet, mobile } = breakpoints;
+
+    const viewport = useMediaQuery(
+        [
+            desktop,
+            laptop,
+            tablet,
+            mobile
+        ],
+        ['desktop', 'laptop', 'tablet', 'mobile'],
+        'mobile'
+    )
+    
     return (
-        <section id="linktab-wrapper">
+        <section className={`${viewport}`} id="linktab-wrapper">
             <span>{props.children}</span>
         </section>
     )
