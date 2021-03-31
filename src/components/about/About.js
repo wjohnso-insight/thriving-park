10f34ad5a329f0093611495b9d9d3615dabc46ee
Will <will.johnston@insight.com>
2021-03-31 18:22:56 -0400

    Adds `CommunityFeed` to `<About>`
    I added a `Commnuity Feed` section to `<About>`. I also added style,
    tests, and snapshots.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,23 +2,39 @@
 import * as React from 'react'
 import axios from 'axios'
 
+import './About.scoped.scss'
+
 type Props = {
 
 }
 
 export default function About(props: Props) : React.Node {
     
-    const [ feedPosts, setFeedPosts] = React.useState()
+    const { useState, useEffect } = React;
+    const [ feedPosts, setFeedPosts] = useState()
 
-    React.useEffect(() => {
+    useEffect(() => {
         axios.get('api/feedPosts')
         .then((res) => setFeedPosts(res.data.feedPosts))
-        .catch((err) => console.log(err))
+        .catch((err) => console.error(err))
     },[])
 
+    const feedPostsRenderer = () => {
+        if(feedPosts){
+            return(
+                feedPosts.map(post => <span key={post.id}>{post.body}</span>)
+            )
+        }else{
+            return <span>Getting posts</span>
+        }
+    }
+    
     return (
-        <div>
-            {(feedPosts ? <span>{feedPosts[0].body}</span> : <span>Getting posts</span>)}
+        <div className="content" id="about-wrapper">
+            <section className="content-panel" id="community-feed">
+                <h1 className="graf sign" data-testid="feed-header">Community Feed</h1>
+                {feedPostsRenderer()}
+            </section>
         </div>
     )
 }
